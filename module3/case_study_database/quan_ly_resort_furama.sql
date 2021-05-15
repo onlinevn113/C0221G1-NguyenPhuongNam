981eec5fe23d96938feda4481f71454756a46bfd
onlinevn113 <onlinevn@gmail.com>
2021-05-15 20:41:26 +0700

    up case study db

--- module3/case_study_database/quan_ly_resort_furama.sql
+++ module3/case_study_database/quan_ly_resort_furama.sql
@@ -2,6 +2,8 @@ DROP DATABASE IF EXISTS quan_ly_furama;
 CREATE DATABASE IF NOT EXISTS quan_ly_furama;
 use quan_ly_furama;
 
+-- 1.Thêm mới thông tin cho tất cả các bảng có trong CSDL để có thể thõa mãn các yêu cầu bên dưới.
+
 create table if not exists vi_tri(
 id_vi_tri int primary key,
 ten_vi_tri varchar(45)
@@ -54,10 +56,10 @@ email varchar(45) unique,
 dia_chi varchar(45)
 );
 insert into nhan_vien 
-value (1,"Nguyễn Phương Nam",1,1,1,"1999-06-27",201807687,4000,0774501871,"onlinevn113@gmail.com","K110/08 Nguyễn Phan Vinh"),
- (2,"Nguyễn",2,2,1,"1999-07-27",201807683,7000,0774501872,"onlinevn@gmail.com","K110/08 Nguyễn Vinh"),
- (3,"Nguyễn Anh",2,2,3,"1999-08-22",201807682,5000,0774501873,"onlinevn1@gmail.com","K110/08 Nguyễn Vinh"),
- (4,"Nguyễn Phương",1,2,3,"2001-06-27",201807611,10000,0774501874,"onlinevn2@gmail.com","K110/08 Nguyễn Phan");
+value (1,"Hùng qwertyuiopsdf ghjklz",1,1,1,"1999-06-27",201807687,4000,0774501871,"onlinevn113@gmail.com","K110/08 Nguyễn Phan Vinh"),
+ (2,"Kiên",2,2,1,"1999-07-27",201807683,7000,0774501872,"onlinevn@gmail.com","K110/08 Nguyễn Vinh"),
+ (3,"Tiến",2,2,3,"1999-08-22",201807682,5000,0774501873,"onlinevn1@gmail.com","K110/08 Nguyễn Vinh"),
+ (4,"Nam",1,2,3,"2001-06-27",201807611,10000,0774501874,"onlinevn2@gmail.com","K110/08 Nguyễn Phan");
 
 
 create table if not exists loai_khach(
@@ -78,13 +80,17 @@ id_loai_khach int,
 foreign key(id_loai_khach) references loai_khach(id_loai_khach),
 ho_ten varchar(45)  not null,
 ngay_sinh date,
-so_cmnd varchar(45) unique,
+so_cmnd varchar(45),
 so_dien_thoai varchar(45),
-email varchar(45) unique,
+email varchar(45),
 dia_chi varchar(45)
 );
--- insert into khach_hang
--- value ();
+insert into khach_hang
+value 
+(1,1,"Huy","1999-12-20",1234567890,0234567890,"qwe@gmail.com","Đà Nẵng"),
+(2,1,"Huy","1999-12-20",0987654321,1234567890,"qwe@gmail.com","Đà Nẵng"),
+(3,1,"Hoàng","1990-3-21",1234561230,1234561230,"we@gmail.com","Quảng trị"),
+(4,2,"Hùng","1990-2-20",1234569870,1234569870,"gwe@gmail.com","Huế");
 
 
 create table if not exists kieu_thue(
@@ -92,16 +98,21 @@ id_kieu_thue int primary key,
 ten_kieu_thue varchar(45),
 gia int
 );
--- insert into kieu_thue
--- value();
-
+insert into kieu_thue
+value
+(1,"Ngày",200),
+(2,"Tuần",300),
+(3,"Tháng",400);
 
 create table if not exists loai_dich_vu(
 id_loai_dich_vu int primary key,
 ten_loai_dich_vu varchar(45)
 );
--- insert into loai_dich_vu
--- value(),
+insert into loai_dich_vu
+value
+(1,"Villa"),
+(2,"House"),
+(3,"Room");
 
 
 create table if not exists dich_vu(
@@ -117,53 +128,123 @@ id_loai_dich_vu int,
 foreign key(id_loai_dich_vu) references loai_dich_vu(id_loai_dich_vu),
 trang_thai varchar(45)
 );
--- insert into dich_vu
--- value ();
+insert into dich_vu
+value
+ (1,"Villa",40,3,10,200,3,2,"available"),
+ (2,"House",30,2,6,200,3,2,"available"),
+ (3,"Room",20,0,4,100,1,2,"available"),
+ (4,"Villa",40,4,14,250,3,1,"unavailable")
+ ;
 
 
 create table if not exists dich_vu_di_kem(
 id_dich_vu_di_kem int primary key,
 ten_dich_vu_di_kem varchar(45),
 gia int,
-don_vi int,
+don_vi varchar(45),
 trang_thai_kha_dung varchar(45)
 );
--- insert into dich_vu_di_kem
--- value();
+insert into dich_vu_di_kem
+value
+(1,"Massage",200,"hour","available"),
+(2,"Room Service",100,"time","available"),
+(3,"Spa",200,"hour","available"),
+(4,"Buggy",300,"time","unavailable")
+;
 
 
 create table if not exists hop_dong(
 id_hop_dong int primary key,
-
 id_nhan_vien int,
 foreign key(id_nhan_vien) references nhan_vien(id_nhan_vien),
-
 id_khach_hang int,
 foreign key(id_khach_hang) references khach_hang(id_khach_hang),
-
 id_dich_vu int,
 foreign key(id_dich_vu) references dich_vu(id_dich_vu),
-
 ngay_lam_hop_dong date,
 ngay_ket_thuc date,
-tien_dat_coc int,
-tong_tieng int
+tien_dat_coc int
 );
--- insert into hop_dong
--- value();
-
+insert into hop_dong
+value
+(1,1,1,1,"2021-10-12","2021-11-12",100),
+(2,2,2,2,"2021-10-12","2021-11-12",100),
+(3,2,3,2,"2018-10-12","2018-11-12",100),
+(4,2,4,1,"2018-10-12","2018-11-12",100),
+(5,2,4,1,"2018-10-12","2018-11-12",100)
+;
 
 create table if not exists hop_dong_chi_tiet(
 id_hop_dong_chi_tiet int primary key,
-
 id_hop_dong int,
 foreign key(id_hop_dong) references hop_dong(id_hop_dong),
-
 id_dich_vu_di_kem int,
 foreign key(id_dich_vu_di_kem) references dich_vu_di_kem(id_dich_vu_di_kem),
-
 so_luong int
 );
--- insert into hop_dong_chi_tiet
--- value();
+insert into hop_dong_chi_tiet
+value
+(1,1,2,3),
+(2,2,2,3),
+(3,3,4,3),
+(4,4,2,3)
+;
+
+
+
+-- 2.Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên 
+-- bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 ký tự.
+select * 
+from nhan_vien
+where (ho_ten_nhan_vien like 'H%' or ho_ten_nhan_vien like 'T%' or ho_ten_nhan_vien like 'K%') and length(ho_ten_nhan_vien)<= 15 ;
+
+-- 3.Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và 
+-- có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+select *
+from khach_hang
+where (year(current_date())-year(ngay_sinh) between 18 and 50) and (dia_chi="Đà Nẵng" or dia_chi="Quảng Trị");
+
+-- 4.Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. 
+-- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+-- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+
+select  khach_hang.ho_ten,khach_hang.so_cmnd,count(hop_dong.id_hop_dong) as "so_lan_dat_phong"
+from khach_hang  
+join hop_dong on khach_hang.id_khach_hang=hop_dong.id_khach_hang
+where khach_hang.id_loai_khach = 1
+group by khach_hang.so_cmnd
+order by "so_lan_dat_phong";
+
+-- 5.Hiển thị IDKhachHang, HoTen, TenLoaiKhach, IDHopDong, 
+-- TenDichVu, NgayLamHopDong, NgayKetThuc, TongTien 
+-- (Với TongTien được tính theo công thức như sau: 
+-- ChiPhiThue + SoLuong*Gia, với SoLuong và Giá là từ bảng DichVuDiKem) 
+-- cho tất cả các Khách hàng đã từng đặt phỏng. 
+-- (Những Khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+
+select kh.id_khach_hang, kh.ho_ten, lh.ten_loai_khach, hd.id_hop_dong, dv.ten_dich_vu,
+hd.ngay_lam_hop_dong, hd.ngay_ket_thuc,sum(dv.chi_phi_thue+hdct.so_luong*dvdk.gia) as tong_tien
+from khach_hang kh
+left join loai_khach lh on kh.id_loai_khach=lh.id_loai_khach
+left join hop_dong hd on kh.id_khach_hang=hd.id_khach_hang
+left join dich_vu dv on hd.id_dich_vu=dv.id_dich_vu
+left join hop_dong_chi_tiet hdct on hd.id_hop_dong=hdct.id_hop_dong
+left join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem=dvdk.id_dich_vu_di_kem
+group by kh.id_khach_hang;
+
+-- 6.Hiển thị IDDichVu, TenDichVu, DienTich, ChiPhiThue, TenLoaiDichVu của tất cả các loại Dịch vụ 
+-- chưa từng được Khách hàng thực hiện đặt từ quý 1 của năm 2019 (Quý 1 là tháng 1, 2, 3).
+select dv.id_dich_vu, dv.ten_dich_vu,dv.dien_tich,dv.chi_phi_thue,dv.ten_dich_vu
+from dich_vu dv
+where dv.id_dich_vu not in (select hop_dong.id_dich_vu
+						from hop_dong 
+                        where (year(ngay_lam_hop_dong)=2019) and (month(ngay_lam_hop_dong) in (1,2,3))
+                       );
+-- 7.	Hiển thị thông tin IDDichVu, TenDichVu, DienTich, SoNguoiToiDa, ChiPhiThue, TenLoaiDichVu 
+-- của tất cả các loại dịch vụ đã từng được Khách hàng đặt phòng trong năm 2018 
+-- nhưng chưa từng được Khách hàng đặt phòng  trong năm 2019.
+select dv.id_dich_vu, dv.ten_dich_vu,dv.dien_tich,dv.chi_phi_thue,dv.ten_dich_vu
+from dich_vu dv
+join hop_dong hd on dv.id_dich_vu=hd.id_dich_vu
+where (year(hd.ngay_lam_hop_dong)=2019);
 
