5916ba58beed2291849b50f0c4bb471d52cabdaf
onlinevn113 <onlinevn@gmail.com>
2021-05-20 15:11:41 +0700

    up bai tap

--- module3/bai_tap_lam_them_internship/db_internship.sql
+++ module3/bai_tap_lam_them_internship/db_internship.sql
@@ -6,7 +6,12 @@ faculty_id int primary key,
 faculty_name char(30),
 phone char(10)
 );
-
+insert into faculty
+value
+(1,'Toan','1234567890'),
+(2,'The Duc','0987654321'),
+(3,'Ngoai Ngu','5432167890')
+;
 create table instructor(
 instructor_id int primary key,
 instructor_name char(30),
@@ -14,6 +19,14 @@ salary int,
 faculty_id int,
 foreign key (faculty_id) references faculty(faculty_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+insert into instructor
+value
+(1,'Nam',200,1),
+(2,'Tùng',300,1),
+(3,'Hieu',400,2)
+;
+
 
 create table student(
 student_id int primary key,
@@ -24,12 +37,29 @@ date_of_birth date,
 place_of_birth char(30)
 );
 
+insert into student
+value
+(1,'Nam Nguyen1',1,'2000-1-1','Hue'),
+(2,'Nam Nguyen2',2,'2000-2-1','Da Nang'),
+(3,'Nam Nguyen3',3,'2000-3-1','Da Nang')
+;
+
+
+
 create table project(
 project_id int primary key,
 project_name char(30),
 expense int,
 place_of_intern char(30)
 );
+
+insert into project
+value
+(1,'VIP1',500,'room1'),
+(2,'VIP2',100,'room2'),
+(3,'VIP3',200,'room3')
+;
+
 
 
 create table instructor_student(
@@ -45,4 +75,10 @@ foreign key (instructor_id) references instructor(instructor_id) ON DELETE CASCA
 grade decimal
 );
 
+insert into instructor_student
+value
+(1,1,1,1,9.5),
+(2,2,1,3,8),
+(3,1,3,2,10)
+;
 
