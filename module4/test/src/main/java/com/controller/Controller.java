f9530f7a99adb658cabbf1733eebae0c334badf1
onlinevn113 <onlinevn@gmail.com>
2021-06-16 11:27:00 +0700

    up bai tap

---
+++ module4/test/src/main/java/com/controller/Controller.java
@@ -0,0 +1,11 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping
+    public String list(){
+        return "list";
+    }
+}
